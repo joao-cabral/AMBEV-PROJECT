93304a510f19b89e12f44fa4333ccfb742ef220e
João Cabral <joaocabral1232@gmail.com>
2022-06-28 23:48:38 -0400

    feat: get all data of beer

--- ambev_scraping/app.js
+++ ambev_scraping/app.js
@@ -6,17 +6,17 @@ const { performance } = require('perf_hooks');
         const browser = await chromium.launch({headless: false, slowMo: 100});
         const page = await browser.newPage();
 
-        // await page.goto('https://www.ambev.com.br/marcas/cervejas/');
+        await page.goto('https://www.ambev.com.br/marcas/cervejas/');
         // await page.goto('https://www.ambev.com.br/marcas/cervejas/caracu/caracu/');
         // await page.goto('https://www.ambev.com.br/marcas/cervejas/budweiser/budweiser/');
         // await page.goto('https://www.ambev.com.br/marcas/cervejas/berrio-do-piaui/berrio-do-piaui/');
-        await page.goto('https://www.ambev.com.br/marcas/cervejas/brahma/brahma-malzibier/');
+        // await page.goto('https://www.ambev.com.br/marcas/cervejas/brahma/brahma-malzibier/');
         await page.waitForTimeout(1000); 
         
         //Click in button "yes"
         await page.click('#age-gate__form > div.age-gate-dialog__body > div.age-gate-dialog__content > div > button');
         
-        // Find all links and save in array
+        // // Find all links and save in array
         const linksBrands = await page.$$eval('.brand__link', elements => {
             const data = [];
             elements.forEach(element => {
@@ -40,11 +40,12 @@ const { performance } = require('perf_hooks');
         await browser.close();
 
         //check if the link has one or more beers
-        listOfLinksWithinBrands.forEach((link) => {
+        listOfLinksWithinBrands.forEach( async (link) => {
             const splitLink = link.split('/');
             if(splitLink[5] === splitLink[6]){
                 const startTime = performance.now();
-                getElementsBeer(link);
+                console.log(link);
+                await getElementsBeer(link);
                 const endTime = performance.now();
                 console.log("Performance GetELementsBeer: " + (endTime - startTime) + "ms");
             }
@@ -66,6 +67,19 @@ async function getElementsBeer(link) {
     await page.goto(link);
     await page.waitForTimeout(1000);
     
+    //Pegar os textos dos ingredientes e alergênicos
+    const allergicAndIngredients = await page.$$eval('body > div.page-wrap > div.internal-page > section.main-section__wrap.product-info > div.product-info__description-wrapper > div.product-info__text-feature.product-info__no-mobile',
+            elements => {
+                const allergicAndIngredients = []
+                elements.forEach(element => {
+                    allergicAndIngredients.push(element.textContent);
+                });
+                return allergicAndIngredients;
+            }
+        );
+    
+    //TODO: Fazer condicionais para quando nao tiver os valores procurados
+
     const name = await page.locator('body > div.page-wrap > div.internal-page > section > div.product-info__description-wrapper > h1 > span').textContent();
     const logo = await page.locator('body > div.page-wrap > div.internal-page > section.main-section__wrap.product-info > div.product-info__description-wrapper > div.product-info__logo-wrapper > a').href;
     const nationality = await page.locator('body > div.page-wrap > div.internal-page > section.main-section__wrap.product-info > div.product-info__description-wrapper > div.product-info__category > div > img').href;
@@ -74,16 +88,15 @@ async function getElementsBeer(link) {
     const ideal_temperature = await page.locator('#product-additional-info > p.product-info__item.product-info__item-icon.product-info__item-icon--ideal-temperature').textContent();
     const ibu =  await page.locator('#product-additional-info > p.product-info__item.product-info__item-icon.product-info__item-icon--ibu').textContent();
     
-    //TODO: testar sem a numeração no final
-    const ingredients = await page.locator('body > div.page-wrap > div.internal-page > section.main-section__wrap.product-info > div.product-info__description-wrapper > div:nth-child(5)').textContent();
-
-    const allergic_ingredients = await page.locator('body > div.page-wrap > div.internal-page > section.main-section__wrap.product-info > div.product-info__description-wrapper > div:nth-child(6)').textContent();
+    const ingredients = allergicAndIngredients[0].split(':')[1];
+    const allergic_ingredients = allergicAndIngredients[1].split(':')[1];
+    
     const nutricional_information = await page.locator('#product-additional-info > p.product-info__item.product-info__item-icon--nutritional-information.product-info__item-nutritional-information.product-info__item-nutritional-information--active').textContent();
     const calories = await page.locator('#product-additional-info > p.product-info__item.product-info__item-icon.product-info__item-icon--calories.product-info__item-calories.product-info__item-calories--active').textContent();
     const carbohydrates = await page.locator('#product-additional-info > p.product-info__item.product-info__item-icon.product-info__item-icon--carbohydrates.product-info__item-carbohydrates.product-info__item-carbohydrates--active').textContent();
     
     //min:2 / max: 173 / step: 19 => n / 171
-    //TODO: implementar condicionais para quando nao tem cor
+    //TODO: implementar condicionais para quando nao tiver cor
     const color = await getColor() / 171;
 
     async function getColor(){
@@ -93,6 +106,24 @@ async function getElementsBeer(link) {
         });
         return color;
     }
+
+    const typeBeer = {
+        name: name,
+        logo: logo,
+        nationality: nationality,
+        description: description,
+        percent_alcohol: percent_alcohol,
+        ideal_temperature: ideal_temperature,
+        ibu: ibu,
+        ingredients: ingredients,
+        allergic_ingredients: allergic_ingredients,
+        nutricional_information: nutricional_information,
+        calories: calories,
+        carbohydrates: carbohydrates,
+        color: color
+    };
+
+    console.log(typeBeer);
 
     await browser.close();
 }
